2281a5f7c520f69c9a95ca0c908f55272f415492
Roberto de la Rosa Antunez <robertoantunez10@gmail.com>
2025-12-10 16:40:49 -0600

    Ya un 80%

---
+++ scripts/calculations.js
@@ -0,0 +1,37 @@
+let budgetValue = 0;
+let totalExpensesValue = 0;
+let balanceColor = "green";
+const expenseEntries = [];
+
+for (let i = 0; i < expenseEntries.length; i++) {
+  const entry = expenseEntries[i];
+  totalExpensesValue += entry[1];
+}
+function calculateBalance() {
+  budgetValue - totalExpensesValue;
+  return;
+}
+function calculateAverageExpense() {
+  if (expenseEntries.length === 0) {
+    return 0;
+  }
+
+  if (totalExpensesValue === 0) {
+    return totalExpensesValue / expenseEntries.length;
+  }
+  function calculateBalance() {
+    return budgetValue - totalExpensesValue;
+  }
+
+  function updateBalanceColor() {
+    const balance = calculateBalance();
+
+    if (balance < 0) {
+      balanceColor = "red";
+    } else if (balance < budgetValue * 0.25) {
+      balanceColor = "orange";
+    } else {
+      balanceColor = "green";
+    }
+  }
+}
